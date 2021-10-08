8da70e043a73fd9708c55ee01ff660acd8ef4059
GeonNamGung <rndrjs123@naver.com>
2021-10-09 04:09:50 +0900

    update homepage layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Input, InputGroup, InputLeftAddon, VStack, Heading } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftAddon, VStack, Heading, Flex, Box } from "@chakra-ui/react";
 import keypair from "../mobx/keypair";
 import { useNetwork } from "../hooks/useNetwork";
 import Spacing from "../components/Spacing/Spacing";
@@ -15,46 +15,44 @@ const Home = () => {
   const [network] = useNetwork();
 
   return (
-    <div>
-      <div>
-        <Heading>내 지갑</Heading>
-        <br />
-        <VStack spacing={4}>
-          <InputGroup>
-            <InputLeftAddon children="지갑 주소(공개키)" />
-            <Input placeholder="from address" variant="filled" name="fromAddr" value={kp.getPublic("hex")} />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftAddon children="개인키" />
-            <Input placeholder="from address" variant="filled" name="fromAddr" value={kp.getPrivate("hex")} />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftAddon children="잔고" />
-            <Input placeholder="from address" variant="filled" name="fromAddr" value={network.blockchain.getBalanceOfAddress(kp.getPublic("hex"))} />
-          </InputGroup>
-        </VStack>
-      </div>
-      <Spacing />
-      <div>
-        <Heading>나의 NFT</Heading>
-        <br />
-        <VStack spacing={4}>
-          <MyNFT blockchain={network.blockchain} walletAddr={kp.getPublic("hex")} />
-        </VStack>
-      </div>
-      <Spacing />
-      <div>
-        <CreateTransaction keyPair={kp} />
-      </div>
-      <Spacing />
-      <div>
-        <Heading>모든 NFT</Heading>
-        <br />
-        <VStack spacing={4}>
+    <Flex>
+      <Box w={"256px"}>
+        <div>
+          <Heading>내 지갑 정보</Heading>
+          <br />
+          <VStack spacing={4}>
+            <InputGroup>
+              <InputLeftAddon children="지갑 주소(공개키)" />
+              <Input placeholder="from address" variant="filled" name="fromAddr" value={kp.getPublic("hex")} />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftAddon children="개인키" />
+              <Input placeholder="from address" variant="filled" name="fromAddr" value={kp.getPrivate("hex")} />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftAddon children="잔고" />
+              <Input placeholder="from address" variant="filled" name="fromAddr" value={network.blockchain.getBalanceOfAddress(kp.getPublic("hex"))} />
+            </InputGroup>
+          </VStack>
+        </div>
+        <Spacing />
+        <div>
+          <Heading>나의 NFT</Heading>
+          <br />
+          <VStack spacing={4}>
+            <MyNFT blockchain={network.blockchain} walletAddr={kp.getPublic("hex")} />
+          </VStack>
+        </div>
+      </Box>
+      <Box w={"32px"}></Box>
+      <Box flex={1}>
+        <div>
+          <Heading>모든 NFT</Heading>
+          <br />
           <AllNFT />
-        </VStack>
-      </div>
-    </div>
+        </div>
+      </Box>
+    </Flex>
   );
 };
 
